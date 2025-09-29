3ffbf781f5d2dc6e565bfc2f33af810b1b8b3f40
sktdr <sktdr.com@gmail.com>
2025-09-29 11:48:27 +0900

    Initial Commit

---
+++ sktdrkana/sktdrkanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sktdrkanaApp.swift
+//  sktdrkana
+//
+//  Created by forte on 2025/09/29.
+//
+
+import SwiftUI
+
+@main
+struct sktdrkanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
